878307164464fb5f5efbdf7f6c09f94df6d22bb9
Enes Sağlam <59392488+enessaglam0180@users.noreply.github.com>
2024-04-24 16:29:25 +0300

    An employee control system

---
+++ Controllers/DepartmanController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PersonelMVC.Models;
+using PersonelMVC.ViewModels;
+
+namespace PersonelMVC.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        public ActionResult Detay(int id)
+        {
+            var departman = new Departman() { Id = id, Ad = "Bilgi işlem" };
+            var personeller = new List<Personel>()
+            {
+                new Personel() { Ad = "Personel1"},
+                new Personel() { Ad = "Personel2"},
+                new Personel() { Ad = "Personel3"}
+            };
+            var model = new DepartmanDetayViewModel()
+            {
+                Departman = departman,
+                Personeller = personeller
+            };
+            return View(model);
+        }
+
+
+    }
+
+}
---
+++ Controllers/PersonelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVC.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Index()
+        {
+            return Content("Personel Anasayfa");
+        }
+
+    }
+}
